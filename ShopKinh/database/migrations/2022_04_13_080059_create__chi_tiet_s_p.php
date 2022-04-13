3501715b2e2efa882c722caab745121c4420ffc7
Nguyễn Nhật Mến <100296653+nguyenNhatMeny@users.noreply.github.com>
2022-04-13 18:53:55 +0700

    oke

---
+++ ShopKinh/database/migrations/2022_04_13_080059_create__chi_tiet_s_p.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('_chi_tiet_s_p', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('san_pham_id');
+            $table->foreignId('loai_sp_id');
+            $table->foreignId('nha_sx_id');
+            $table->string('ten_sp', 255);
+            $table->string('gia', 50);
+            $table->string('chat_lieu', 100)->nullable();
+            $table->string('mau_sac', 100)->nullable();
+   
+            $table->integer('so_luong')->default(0);
+            $table->string('giam_gia',10)->default(0);
+            $table->longText('hinh_anh')->nullable();
+            $table->longText('mo_ta')->nullable();
+            $table->integer('new')->default(0);
+            $table->boolean('tinh_trang')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('_chi_tiet_s_p');
+    }
+};
---
+++ ShopKinh/database/migrations/2022_04_13_080627_create__loai_s_p.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('_loai_s_p', function (Blueprint $table) {
+            $table->id();
+            $table->string('ten');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('_loai_s_p');
+    }
+};
